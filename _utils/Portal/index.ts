f60e984a3ecbac060d72ece579b3460644daf267
杨浩 <baotou5215@qq.com>
2021-11-06 00:37:16 +0800

    初始化

--- _utils/Portal/index.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Autor: Yhao
- * @Date: 2021-04-11 16:29:26
- * @LastEditors: Yhao
- * @LastEditTime: 2021-04-11 16:29:47
- * @Description:
- */
--- _utils/types.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Autor: Yhao
- * @Date: 2021-04-11 16:46:05
- * @LastEditors: Yhao
- * @LastEditTime: 2021-04-11 16:46:31
- * @Description: type类型工具
- */
-
-export const tuple = <T extends string[]>(...args: T) => args;
